d0e1a982438d6ddeff9d5fede0cfbb71e611721f
NASEELA K T <naslalellu@gmail.com>
2024-06-25 12:32:27 +0530

    port changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const axios=require("axios")
 
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 const adminRoute=require("./routes/adminRoute")
 const userRoute = require("./routes/userRoute")
